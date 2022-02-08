bbc311a2be5ef237a76bdf0fe353473b06e47172
Khaled <Khaled-Shbair>
2022-02-08 15:17:06 +0200

    one

--- lib/Screen_Bio.dart
+++ lib/Screen_Bio.dart
@@ -69,23 +69,43 @@ class Screen_Bio extends StatelessWidget {
                 indent: 40,
                 endIndent: 40,
               ),
-              CardBio(
-                leadingIcon: Icons.account_circle,
-                onPressedFunction: () {
-                  print('Full name: Khaled Shbair');
-                },
-                title: 'Full name',
-                subtitle: 'Khaled Shbair',
-                marginBottom: 10,
-              ),
-              CardBio(
-                leadingIcon: Icons.collections_bookmark_outlined,
-                onPressedFunction: () {
-                  print('Course Name: Flutter');
-                },
-                title: 'Course Name',
-                subtitle: 'Flutter',
-                marginBottom: 10,
+              Container(
+                margin: const EdgeInsetsDirectional.only(
+                    start: 40, end: 40, bottom: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withAlpha(41),
+                      offset: const Offset(0, 3),
+                      blurRadius: 6,
+                    ),
+                  ],
+                ),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.account_circle,
+                    size: 30,
+                  ),
+                  title: const Text(
+                    'Engineer',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  subtitle: const Text(
+                    'Momen M.Reyad Sisalem',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  onTap: () {
+                    print('Engineer: Momen M.Reyad Sisalem');
+                  },
+                ),
               ),
               CardBio(
                 leadingIcon: Icons.email,
@@ -100,25 +120,50 @@ class Screen_Bio extends StatelessWidget {
                 leadingIcon: Icons.phone,
                 onPressedFunction: () {
                   print('Full name: +970599724037');
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: const Text(
+                      'Send call',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                    backgroundColor: Colors.white,
+                    duration: const Duration(seconds: 3),
+                    behavior: SnackBarBehavior.floating,
+                    margin: const EdgeInsetsDirectional.all(20),
+                    dismissDirection: DismissDirection.horizontal,
+                    padding: const EdgeInsetsDirectional.all(10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    action: SnackBarAction(
+                      label: 'ok',
+                      onPressed: () {
+                        print('OK');
+                      },
+                      textColor: Colors.blue,
+                    ),
+                  ));
                 },
                 title: 'Mobile',
                 subtitle: '+970599724037',
+                marginBottom: 60,
+              ),
+              const PositionedDirectional(
+                start: 20,
+                end: 20,
+                bottom: 20,
+                child: Text(
+                  'Flutter - 2022',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
               ),
             ],
-          ),
-          const PositionedDirectional(
-            start: 20,
-            end: 20,
-            bottom: 20,
-            child: Text(
-              'Flutter - 2022',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.yellow,
-                fontSize: 15,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
           ),
         ],
       ),
